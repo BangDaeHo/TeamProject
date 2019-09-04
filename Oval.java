ac102bdabf6687e69489f74ef1af4f0cd08f9e71
BangDaeHo <hanava1@naver.com>
2019-09-04 14:32:25 +0900

    Oval클래스 추가

---
+++ Oval.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Oval here.
+ *
+ * @author (2018315021 방대호)
+ * @version (2019.09.04)
+ */
+public class Oval implements Shape 
+{
+    int x, y;    
+    public Oval(int x , int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(){
+        System.out.println(x + "x" + y + "에 내접하는 타원입니다.");
+    }
+
+    public double getArea(){
+        return x * y * PI;
+    }
+}
