19348b2eaab5f71fb1a1cbc9097ac05629af0cb4
태영준 <ytae1999@naver.com>
2019-09-04 14:54:42 +0900

    학번 수정.

--- Rect.java
+++ Rect.java
@@ -2,7 +2,7 @@
 /**
  * 여기에 Rect 클래스 설명을 작성하십시오.
  * 
- * @author (2018315021 태영준 ,2018315051 yamamoto yoshika , 2018315021 방대호) 
+ * @author (2018315021 태영준 , 2018315051 yamamoto yoshika , 2018315021 방대호) 
  * @version (2019.09.04)
  */
 public class Rect implements Shape
